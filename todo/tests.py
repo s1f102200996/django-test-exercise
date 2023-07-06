01f72c5e9213d4b5611d1b9aa1c68ad541190fd8
1f10220099 <s1f102200996@iniad.org>
2023-07-06 16:02:02 +0900

    編集4

--- todo/tests.py
+++ todo/tests.py
@@ -38,7 +38,8 @@ class TaskModelTestCase(TestCase):
         task = Task(title="task1", due_at=due)
         task.save()
 
-        self.assertFalse(task.is_overdue(current))
+        self.assertFalse(
+            task.is_overdue(current))
 
     def test_is_over_due_past(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
@@ -47,16 +48,16 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertTrue(
-            task.is_overdue(timezone.make_aware(current)))
+            task.is_overdue(current))
 
     def test_is_overdue_none(self):
-        due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
+        due = None
         current = timezone.make_aware(datetime(2023, 7, 1, 0, 0, 0))
-        task = Task(title="task1")
+        task = Task(title="task1",due_at = due)
         task.save()
 
         self.assertFalse(
-            task.is_overdue(timezone.make_aware(current)))
+            task.is_overdue(current))
 
 
 class TodoViewTestCase(TestCase):
